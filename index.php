48aabbace1b7c78309fab5ed7bd6d75781a52a1b
= <=>
2022-03-09 12:29:31 +0800

    form validation part 1

--- index.php
+++ index.php
@@ -1,79 +1,32 @@
-<?php
-
-use FFI\ParserException;
-$break = "<br>";
-class User {
-    public $userName;
-    protected $email;
-    public $role = "member";
-
-    public function __construct($username, $email) {
-        $this->userName = $username;
-        $this->email = $email;
-
-    }
-    //this function is like the opposite of the construct method
-    public function __destruct() {
-        echo "$this->email is removed <br/>";
-    }
-    //this magic function will perform when there is new instance is created using clone
-    public function __clone() {
-        $this->userName = $this->userName . "(cloned) <br/>";
-    }
-    public function message() {
-        return "$this->email sent a new message";
-    }
-    public function getEmail() {
-        return $this->email;
-    }
-    public function changeEmail($email) {
-        if (strpos($email, "@") > -1) {
-            $this->email = $email;
-        }
-    }
-
+<?php  
+if (isset($_POST['submit'])) {
+    echo "Hello World";
 }
-class adminUser extends User {
-    public $level;
-    public $role = "admin";
-
-    public function __construct($username, $email, $level) {
-        $this->level = $level;
-        parent::__construct($username, $email);
-        
-    }
-
-    public function message() {
-        return "$this->email, an admin, sent a message";
-    }
-}
-
-$userOne = new User("Gabe", "gabefletchers@gmail.com");
-$userTwo = new User("Alrose", "alrosewasawas@gmail.com");
-$userThree = new adminUser("admin", "theadmin@gmail.com", 6);
-
-$userFour = clone $userOne;
-
-echo $userFour->userName . "<br/>";
-
-
-
-
-
-
-
-
-
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP OOP FORM TUTORIAL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1>Clone and Destruct methods</h1>
+    <div class="new-user">
+        <h2>Create new user</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']?> " method="post">
+
+            <label for="username">Username: </label>
+            <input type="text" name="username">
+
+            <label for="email">Email: </label>
+            <input type="text" name="email">
+
+            <input type="submit" value="Submit" name="submit">
+
+
+        </form>
+    </div>
 </body>
 </html>
---
+++ user_validator.php
@@ -0,0 +1,56 @@
+<?php 
+
+class UserValidator {
+    //variable that holds the data of the user
+    private $data;
+    //if there's an error we can push it to this and display it
+    private $error = [];
+
+    private static $fields = ['username', 'email'];
+
+    public function __construct($postData) {
+    $this->data = $postData;
+        
+    }
+    public function validateForm () {
+        foreach(self::$fields as $field) {
+            if (!array_key_exists($field, $this->data)) {
+                trigger_error("$field is not present in the data");
+                return;
+            }
+        }
+
+        $this->validateUsername();
+        $this->validateEmail();
+        return $this->error;
+    }
+    private function validateUsername() {
+        //trim method clears the white space
+        $value = trim($this->data['username']);
+
+        if (empty($value)) {
+            $this->addError('username', 'username cannot be empty');
+        } else {
+            if(!preg_match('/^[a-zA-Z0-9]{6,12}$/', $value)){
+                $this->addError('username','username must be 6-12 chars & alphanumeric');
+            }
+        }
+    }
+    private function validateEmail() {
+        $value = trim($this->data['email']);
+
+        if (empty($value)) {
+            $this->addError('email', 'email cannot be empty');
+        } else {
+            if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
+                $this->addError('email','email must be a valid email');
+            }
+        }
+    }
+    private function addError($key, $val) {
+        $this->error[$key] = $val;
+    }
+
+}
+
+ ?>
